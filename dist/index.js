f2b5e79c1db1003d994adbc51b0884a0f2a08f24
Shaun Egan <shauneganza@gmail.com>
2021-01-14 16:51:11 +0200

    Authenticate internally

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,26 @@ var core_1 = require("@actions/core");
 var github_1 = require("@actions/github");
 var hotloop_sdk_1 = require("@hotloop/hotloop-sdk");
 var ConfigFactory_1 = require("./ConfigFactory");
+var google_auth_library_1 = require("google-auth-library");
+var getAuthToken = function () {
+    var keysEnvVar = process.env['HOTLOOP_KEY'];
+    if (!keysEnvVar)
+        throw new Error('No hotloop keys provided');
+    var gcloudClient = google_auth_library_1.auth.fromJSON(JSON.parse(keysEnvVar) || '{}');
+    return gcloudClient.getAccessToken()
+        .then(function (result) {
+        if (!result.token)
+            throw new Error('Not authorized');
+        return result.token;
+    });
+};
 var syncCoverage = function (config) {
-    var opts = { userAgent: 'coverage-action', timeout: 5000, retries: 3, retryDelay: 1000 };
-    var client = hotloop_sdk_1.HotLoopSdkFactory.getInstance(config.token, opts);
-    return client.syncCoverage(config.options);
+    return getAuthToken()
+        .then(function (token) {
+        var opts = { userAgent: 'coverage-action', timeout: 5000, retries: 3, retryDelay: 1000 };
+        var client = hotloop_sdk_1.HotLoopSdkFactory.getInstance(token, opts);
+        return client.syncCoverage(config.options);
+    });
 };
 var setFailure = function (error) { return core_1.setFailed(error.message); };
 var logCorrelation = function (correlationId) { return core_1.info("Correlation ID: " + correlationId); };
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import { getInput, info, setFailed } from '@actions/core'
 import { context } from '@actions/github'
 import { CorrelationId, HotLoopSdkFactory } from '@hotloop/hotloop-sdk'
 import { Config, ConfigFactory } from './ConfigFactory'
+import { GoogleAuth } from 'google-auth-library'
+
+const getAuthToken = () => {
+  const keysEnvVar = process.env['HOTLOOP_KEY']
+  if (!keysEnvVar) throw new Error('No hotloop keys provided')
+  const credentials = JSON.parse(keysEnvVar)
+
+  const targetAudience = 'https://europe-west3-hotloop-289416.cloudfunctions.net'
+  const auth = new GoogleAuth({ credentials })
+  return auth.getIdTokenClient(targetAudience)
+    .then(client => client.idTokenProvider.fetchIdToken(targetAudience))
+}
 
 const syncCoverage = (config: Config) => {
-  const opts = { userAgent: 'coverage-action', timeout: 5000, retries: 3, retryDelay: 1000 }
-  const client = HotLoopSdkFactory.getInstance(config.token, opts)
-  return client.syncCoverage(config.options)
+  return getAuthToken()
+    .then(token => {
+      const opts = { userAgent: 'coverage-action', timeout: 5000, retries: 3, retryDelay: 1000 }
+      const client = HotLoopSdkFactory.getInstance(token, opts)
+      return client.syncCoverage(config.options)
+    })
 }
 
 const setFailure = (error: Error) => setFailed(error.message)
